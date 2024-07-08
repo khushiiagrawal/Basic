21eb31153d300bb1a7da5368cb3a15a569164570
Khushi Agrawal <khushisaritaagrawal@gmail.com>
2024-07-08 11:33:07 +0530

    added a basic operation

---
+++ index.js
@@ -0,0 +1,11 @@
+const PI=3.14
+let radius;
+let circumference;
+
+document.getElementById("mySubmit").onclick=function(){
+    radius=document.getElementById("myText").value;
+    radius=Number(radius);
+    circumference=2*PI*radius;
+    document.getElementById("myh3").textContent=circumference;
+
+}
